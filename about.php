d6e95e8d01454ec2642818b9ca93f9136e9a5425
adekartono <34999469+adekartono@users.noreply.github.com>
2018-01-06 21:54:28 +0700

    about [PROJEK 1]

---
+++ about.php
@@ -0,0 +1,16 @@
+    <div class="row" style="padding: 50px 0px 50px 0px; background-color: #2b7cff">
+        <div class="col-sm-12" align="left">
+            <div class="container" style="box-shadow: 0px 0px 15px 0px black; background-color: white; padding: 50px 50px 50px 50px">
+                <legend> Tentang Kami </legend>
+                <font style="font-size: 20px" face="Century Ghotic">
+                    Selamat datang pada website kami,<br>
+                    Sistem Penyewaan Orkes Dangdut Indramayu adalah salah satu akses termudah bagi masyarakat untuk memperoleh informasi dan ataupun mempermudah dalam hal penyewaan suatu Grup Orkes Dangdut.<br><br>
+                    PROSES MUDAH, AMAN DAN NYAMAN !<br>
+                    PASTI EFEKTIF !<br>
+                    PASTI DATANG !<br><br>
+                    Kami juga memberi kesempatan kepada Grup Orkes Dangdut khususnya pada wilayah Kabupaten Indramayu untuk bisa mengembangkan pasarnya melalui sistem kami dengan cara bergabung sebagai penyedia agar Grup Orkes Dangdutnya dapat di akses oleh masyarakat luas baik informasinya maupun proses pemesanannya.<br><br>
+                    <b>Let's Join ON !</b>
+                </font>
+            </div>
+        </div>
+    </div>
