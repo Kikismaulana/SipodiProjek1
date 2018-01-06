17c5ffb095bb39931856cbcfe5b6b65ec3512d6e
NunungNurhayati <34527497+NunungNurhayati@users.noreply.github.com>
2018-01-06 21:56:22 +0700

    contact [PROJEK 1]

---
+++ contact.php
@@ -0,0 +1,15 @@
+    <div class="row" style="padding: 50px 0px 50px 0px; background-color: #2b7cff">
+        <div class="col-sm-12" align="left">
+            <div class="container" style="box-shadow: 0px 0px 15px 0px black; background-color: white; padding: 50px 50px 50px 50px">
+                <legend> Kontak Kami </legend>
+                <font style="font-size: 20px" face="Century Ghotic">
+                    <b>Selamat datang pada website kami,</b><br>
+                    Jika terdapat pertanyaan yang berkaitan dengan sistem yang telah kami buat ini, sekiranya anda dapat menghubungi kami melalui kontak yang telah kami sediakan dibawah ini :<br><br>
+
+                    <img src="gambar//phone.png" width="50px" height="50px"> 089509978533 <br><br>
+                    <img src="gambar//email.png" width="50px" height="50px"> sipodi2017@gmail.com <br><br>
+                    <img src="gambar//alamat.png" width="50px" height="50px"> Jalan Raya Lohbener Lama No. 8 <br><br>
+                </font>
+            </div>
+        </div>
+    </div>
